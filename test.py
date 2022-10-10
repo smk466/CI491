a1bf79a7955174dbd93f8fca5303ddb0f0fdccda
gtd32 <gtd32@drexel.edu>
2022-10-09 20:41:08 -0400

    gd Extract content from webpages

--- test.py
+++ test.py
@@ -11,25 +11,87 @@ except ImportError:
 query = "Software Engineering+People"
 
 
-results = search(query, tld="co.in", num=50, stop=50, pause=2)
+#link = search(query, tld="co.in", num=1, stop=1, pause=2)
+#res = requests.get(link)
+
+#print(res.text)
+#print(res.status_code)
+
+
+
+#for i in range(4):
+#    links.append(results[i])
+
+##GET LINKS FROM SEARCH QUERY:
 links = []
-for i in range(4):
-    links.append(results[i])
-
-links2 = []
-for i in range(5-9):
-    links2.append(results[i])
+for j in search(query, tld="co.in", num=2, stop=2, pause=2):
+   links.append(j)
 
 
-
+##ACHIEVE  CONTENT:
+page_title =[] 
+page_body =[]
+page_head = []
+content = []
+names = []
 for i in links:
-    google_result = requests.get(i).text
-    with open("output.txt", "a") as f:
-        print(google_result, file=f)
+    page = requests.get(i)
+    soup = BeautifulSoup(page.content, features = 'html.parser')
+    #Extract title of the page:
+    page_title.append(soup.title.text) 
+    #Extract body of the page:
+    page_body.append(soup.body)
+    # Extract head of page
+    page_head.append(soup.head)
+    #product = soup.select('div.thumbnail')
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    content.append(text)
+
+    
+print(len(content))
+#find names
+import re
+name_regex = re.compile(r"^([A-Za-z \-]{2,25})+$", re.MULTILINE)
+#words = soup.find(text=lambda text: text and name_regex in text)
+for i in content:
+    names.append(name_regex.findall(i))
+print(names)
+
+#print(page.text)
+# print(page.status_code) 
 
 
-for i in links2:
-    google_result = requests.get(i).text
-    with open("output.txt", "a") as f:
-        print(google_result, file=f)
+
+
+
+#links2 = []
+#for i in range(5-9):
+ #   links2.append(results[i])
+
+
+
+#for i in links:
+ #   google_result = requests.get(i).text
+ #   with open("output.txt", "a") as f:
+  #      print(google_result, file=f)
+
+
+#for i in links2:
+ #   google_result = requests.get(i).text
+  #  with open("output.txt", "a") as f:
+   #     print(google_result, file=f)
+
+
+
 
