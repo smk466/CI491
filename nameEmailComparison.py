1f37d8b29d2a584cfa27a762956add867dbfdc4b
khalidsalem <kfsalem2000@gmail.com>
2022-10-26 17:06:40 -0400

    Email and Name Comparison

---
+++ nameEmailComparison.py
@@ -0,0 +1,24 @@
+from difflib import SequenceMatcher
+
+nameList = ["Chris Bryant", "Michael Jordan", "Michael Jackson", "Kobe Bryant"]
+emailList = ["C.Bryant@gmail.com","Kobeeeeee.B@gmail.com","Mich.Jackson@gmail.com","Mich.J@gmail.com"]
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def compareLists(nameList, emailList):
+    for i in nameList:
+        x = 0
+        email = ''
+        for j in emailList:
+            temp = SequenceMatcher(None, i, j).ratio()
+            if temp > x:
+                x = temp
+                email = j
+        print(i,"---",email)
+        emailList.remove(email)        
+
+compareLists(nameList, emailList)
+
+# print(similar("Apple", "Bapple"))
