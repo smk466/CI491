6c2c6e2cc949c24448a0add3befa661be17cb249
khalidsalem <kfsalem2000@gmail.com>
2022-11-07 21:09:08 -0500

    Updates to nameEmailComp

--- nameEmailComparison.py
+++ nameEmailComparison.py
@@ -1,7 +1,7 @@
 from difflib import SequenceMatcher
 
 nameList = ["Chris Bryant", "Michael Jordan", "Michael Jackson", "Kobe Bryant"]
-emailList = ["C.Bryant@gmail.com","Kobeeeeee.B@gmail.com","Mich.Jackson@gmail.com","Mich.J@gmail.com"]
+emailList = ["C.Bryant@gmail.com","Kobe.Br@gmail.com","Mich.Jackson@gmail.com","Mich.J@gmail.com"]
 
 
 def similar(a, b):
@@ -12,12 +12,11 @@ def compareLists(nameList, emailList):
         x = 0
         email = ''
         for j in emailList:
-            temp = SequenceMatcher(None, i, j).ratio()
+            temp = SequenceMatcher(lambda x: x == " ", i, j.split("@")[0]).ratio()
             if temp > x:
                 x = temp
-                email = j
-        print(i,"---",email,"---",x)
-        emailList.remove(email)        
+                email = j   
+        print(i,"---",email,"---",x)        
 
 compareLists(nameList, emailList)
 
