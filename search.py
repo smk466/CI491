2442184a5169ea2a74aa05157d6e84ae4ab9ce1c
SuperChrisBoy <lele2000213@gmail.com>
2022-12-07 00:00:53 -0500

    Search function is now partially working

--- search.py
+++ search.py
@@ -3,7 +3,8 @@ import tkinter.ttk as ttk
 import csv
 
 root = Tk()
-root.title("Python - Import CSV File To Tkinter Table")
+root.title("Python - Search CSV")
+fram = Frame(root)
 width = 900
 height = 600
 screen_width = root.winfo_screenwidth()
@@ -13,41 +14,6 @@ y = (screen_height/2) - (height/2)
 root.geometry("%dx%d+%d+%d" % (width, height, x, y))
 root.resizable(0, 0)
 
-
-TableMargin = Frame(root, width=500)
-TableMargin.pack(side=TOP)
-scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
-scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
-tree = ttk.Treeview(TableMargin, columns=("Link", "Name", "Email", "Specialty"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
-scrollbary.config(command=tree.yview)
-scrollbary.pack(side=RIGHT, fill=Y)
-scrollbarx.config(command=tree.xview)
-scrollbarx.pack(side=BOTTOM, fill=X)
-tree.heading('Link', text="Link", anchor=W)
-tree.heading('Name', text="Name", anchor=W)
-tree.heading('Email', text="Email", anchor=W)
-tree.heading('Specialty', text="Specialty", anchor=W)
-tree.column('#0', stretch=NO, minwidth=0, width=0)
-tree.column('#1', stretch=NO, minwidth=0, width=200)
-tree.column('#2', stretch=NO, minwidth=0, width=200)
-tree.column('#3', stretch=NO, minwidth=0, width=300)
-tree.pack()
-
-with open('output.csv') as f:
-    # reader = csv.DictReader(f, delimiter=',')
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        if len(row) == 0:
-            continue
-        link: str = row[0]
-        name: str = row[1]
-        email: str = row[2]
-        specialty: str = row[3]
-        tree.insert("", 0, values=(link, name, email, specialty))
-
-root = Tk()
-root.geometry("%dx%d+%d+%d" % (width, height, x/2, y/2))
-root.title("Python - Search CSV")
 Searchbar = Entry(root)
 Searchbar.pack()
 SearchString = StringVar(Searchbar, "")
@@ -55,6 +21,10 @@ Searchbar.config(textvariable = SearchString)
 
 SearchResultBar = Label(root)
 SearchResultBar.pack()
+
+butt = Button(fram, text='Find') 
+butt.pack(side=RIGHT)
+fram.pack(side=TOP)
 
 def DisplaySearchResult(event):
     Keyword = SearchString.get()
@@ -91,7 +61,7 @@ def DisplaySearchResult(event):
                 email: str = row[2]
                 specialty: str = row[3]
                 resultTree.insert("", 0, values=(link, name, email, specialty))
-    
+
     SearchString.set(resultTree)
 
     # Step 1: Search through tree with getter and Keyword
@@ -104,14 +74,9 @@ def DisplaySearchResult(event):
     #     if Keyword.lower() in tree.item(child)['values'].lower():
     #         resultTree.insert("", 0, values=(link, name, email, specialty))
 
-
 Searchbar.bind('<Return>', DisplaySearchResult, add = '+')
 Searchbar.bind('<FocusOut>', DisplaySearchResult, add = '+')
+print(SearchString)
 
-def generatecode(self):
-    pass  # Do stuff here
-
-
-#============================INITIALIZATION==============================
 if __name__ == '__main__':
     root.mainloop()
